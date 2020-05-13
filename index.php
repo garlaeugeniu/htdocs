717e1f8bb238c81f382d001a409a2c283fc3fa35
garlaeugeniu <64921943+garlaeugeniu@users.noreply.github.com>
2020-05-13 19:58:44 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,153 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>PHP WEB SITE</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <style>
+  span {
+    display: inline-block;
+    width: 68px;
+    height: 68px;
+    margin: 6px;
+    background-color: rgb(17, 48, 143);
+  }
+  </style>
+</head>
+<body>
+
+<div class="container">
+<p class="bg-info text-white">       INFORMATIE IMPORTANTA PENTRU BUSINESS !!! ATENTIE LA PETURI !!! OFERTA DE NEREFUZAT !!! CONTACTATI-NE   IMEDIAT !!! tel.: (373) 022 112233</p>
+  <h3>ELEMENTE de PROIECTARE la un SITE, utilizand platforma BOOTSTRAP (versiunea a 4-a)</h3>
+  <p></p> 
+  <span class="rounded-bottom"></span>
+  <span class="rounded-lg"></span>
+  <span class="rounded-top"></span>
+  <span class="rounded-right"></span>
+  <span class="rounded-bottom"></span>
+  <span class="rounded-left"></span>
+  <span class="rounded-circle"></span>
+  <span class="rounded-0"></span>
+  <span class="rounded-sm"></span>
+<span class="rounded"></span>
+<span class="rounded-lg"></span>
+<span class="rounded-top"></span>
+<span class="rounded-right"></span>
+</div>
+<p class="text-center"
+> 🔨 🔧 🛠 ANTET FIRMA GARLA SRL🛠 🔧 🔨
+<img src="Photo.pdf" class="img-thumbnail" alt="" width="34" height="34"> 
+</div>
+</p>
+
+<?php
+echo'<br> <a href="style1.css"> </a>';
+date_default_timezone_set('Europe/CHISINAU');
+echo date('l');
+echo '<br />'. date('j-F-Y');
+echo '<br />'. date('H:i:s');
+?>
+
+<body>
+  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+<h5 class="my-0 mr-md-auto font-weight-normal">GARLA SRL</h5>
+<nav class="my-2 my-md-0 mr-md-3">
+  <a class="p-2 text-dark" href="#">GENERAL</a>
+  <a class="p-2 text-dark" href="#">STATISTICA</a>
+  <a class="p-2 text-dark" href="#">SERVICII</a>
+  <a class="p-2 text-dark" href="#">PRETURI</a>
+</nav>
+<a class="btn btn-outline-primary" href="#">INPUT</a>
+</div>
+
+<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+<h1 class="display-4">PRETURI LA PRODUSE</h1>
+<p class="lead"></p>
+</div>
+
+<div class="container">
+<div class="card-deck mb-3 text-center">
+  <div class="card mb-4 shadow-sm">
+    <div class="card-header">
+      <h4 class="my-0 font-weight-normal">LIBERE</h4>
+    </div>
+    <div class="card-body">
+      <h1 class="card-title pricing-card-title">$0 <small class="text-muted"> </small></h1>
+      <ul class="list-unstyled mt-3 mb-4"></ul>
+      <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up</button>
+      <img src="imagine003.png" 
+alt =" NU ESTE" style = "width=120px; height:120px;">
+    </div>
+  </div>
+  <div class="card mb-4 shadow-sm">
+    <div class="card-header">
+      <h4 class="my-0 font-weight-normal">PARTENERI</h4>
+    </div>
+    <div class="card-body">
+      <h1 class="card-title pricing-card-title">$15 <small class="text-muted" </small></h1>
+      <ul class="list-unstyled mt-3 mb-4"></ul>
+      <button type="button" class="btn btn-lg btn-block btn-primary">Start</button>
+      <img src="imagine002.png" 
+alt =" NU ESTE" style = "width=120px; height:120px;">
+    </div>
+  </div>
+  <div class="card mb-4 shadow-sm">
+    <div class="card-header">
+      <h4 class="my-0 font-weight-normal">CONTRACTE</h4>
+    </div>
+    <div class="card-body">
+      <h1 class="card-title pricing-card-title">$30 <small class="text-muted"> </small></h1>
+      <ul class="list-unstyled mt-3 mb-4"></ul>
+      <button type="button" class="btn btn-lg btn-block btn-primary">Contacte</button>
+    </div>
+
+    <img src="imagine001.png" 
+    alt =" NU ESTE" style = "width=120px; height:120px;">
+  </div>
+</div>
+
+<footer class="pt-4 my-md-5 pt-md-5 border-top">
+  <div class="row">
+    <div class="col-12 col-md">
+      
+      <small class="d-block mb-3 text-muted">© 2020-2022</small>
+    </div>
+    <div class="col-6 col-md">
+      <h5>GENERALE</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="#">STUFF</a></li>
+        <li><a class="text-muted" href="#">STRUCTURA</a></li>
+        <li><a class="text-muted" href="#">ECHIPA</a></li>
+        <li><a class="text-muted" href="#">CERCETARE</a></li>
+        <li><a class="text-muted" href="#">ALTE DATE</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>RESURSE</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="#">RESURSE FINANCIARE</a></li>
+        <li><a class="text-muted" href="#">RESURSE MATERIALE</a></li>
+        <li><a class="text-muted" href="#">ALTE RESURSE</a></li>
+        <li><a class="text-muted" href="#">TOTAL RESURSE</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>DESPRE</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="#">ECHIPA</a></li>
+        <li><a class="text-muted" href="#">LOCATIE</a></li>
+        <li><a class="text-muted" href="#">PRIVAT</a></li>
+        <li><a class="text-muted" href="#">CLAUZE</a></li>
+      </ul>
+    </div>
+  </div>
+</footer>
+</div>
+
+</body>
+</html>
